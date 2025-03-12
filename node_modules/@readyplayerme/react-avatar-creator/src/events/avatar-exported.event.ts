5f24ee0c48b9fbde3dc899d8df69db71314bb5c8
koushikreddyvayalpati <72864227+koushikreddyvayalpati@users.noreply.github.com>
2025-03-12 00:07:24 -0400

    Delete node_modules directory

--- node_modules/@readyplayerme/react-avatar-creator/src/events/avatar-exported.event.ts
+++
@@ -1,9 +0,0 @@
-import { IFrameEvent } from '../types';
-
-export type AvatarExportedEventPayload = {
-  url: string;
-  userId: string;
-  avatarId: string;
-};
-
-export type AvatarExportedEvent = IFrameEvent<AvatarExportedEventPayload>;
